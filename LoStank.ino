7961f004d51e7f9303cbff1c964ac77975ea69d0
Robert Eells <eellsrm@gmail.com>
2019-05-15 18:29:34 -0400

    Update LoStank.ino
    first successful compilation of code. not yet tested on hardware

--- LoStank.ino
+++ LoStank.ino
@@ -6,25 +6,26 @@
 #include <PS2Keyboard.h>
 
 //keyboard input
-const int DataPin = ;
-const int IRQpin =  ;
+const int DataPin = 17;
+const int IRQpin =  16;
 
 //ir input
-int RECV_PIN = ;
+int RECV_PIN = 19;
 
 //motor pins
-const int ldir = ;
-const int rdir = ;
-const int lstep = ;
-const int rstep = ;
-const int lfault = ;
-const int rfault = ;
+const int ldir = 50;
+const int rdir = 51;
+const int lstep = 24;
+const int rstep = 25;
+const int gstep = 22; //this pin goes to BOTH step pins through diodes
+const int lfault = 52;
+const int rfault = 53;
 
 //timing and motion variables
 unsigned long oldtime = 0;
 unsigned long ntime;
 unsigned long spd = 25; // milliseconds
-bool lmov;
+bool lmov = 0;
 //bool rmov; //might reimpliment individual toggles
 
 
@@ -44,21 +45,47 @@ void setup() {
  pinMode(lfault,OUTPUT);
  pinMode(rfault,OUTPUT);
 
+ digitalWrite(lfault, LOW); //default steppers to off
+ digitalWrite(rfault, LOW);
+ digitalWrite(ldir, LOW);
+ digitalWrite(rdir, LOW); //direction assignments are kindof arbitrary
+
  
 }
 
 void loop() {
-  ntime = millis();
-  look();
-  if(lmov == TRUE){
+  ntime = millis();     //update current time
+  look();               //check for user input
+  if(lmov == 1){     //check motion flag
    go();
   }
-  rst();
+  rst();                //reset step pins
 }
 
 void look(){
   if (keyboard.available){
-    char c = keyboard.read();
+    char c = keyboard.read();//read keyboard input
+    
+    //q and e toggle fault pins for left and right motor
+    if (c == 'q'){
+      digitalWrite(lfault, !digitalRead(lfault));
+    }
+    if (c == 'e'){
+      digitalWrite(rfault, !digitalRead(rfault));
+    }
+
+    //z and c toggle the direction pins for left and right motors
+    if (c == 'z'){
+      digitalWrite(ldir, !digitalRead(ldir));
+    }
+    if (c == 'c'){
+      digitalWrite(rdir, !digitalRead(rdir));
+    }
+
+    if (c == 'w'){  //stop/go
+      lmov = !lmov;
+    }
+
     
 
   }
@@ -68,12 +95,13 @@ void look(){
 void go(){
   if (ntime - oldtime >= spd){
     oldtime = ntime;
-    digitalWrite(lstep, HIGH);
+    digitalWrite(gstep, HIGH);
     }
 }
 
 void rst(){
+  //reset all step pins to low
   digitalWrite(lstep, LOW);
   digitalWrite(rstep, LOW);
+  digitalWrite(gstep, LOW);
 }
-
